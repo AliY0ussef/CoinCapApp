948cafe79aadf049309528f35d62357897bb9c88
96Ali96 <ali_y0ussef@outlook.com>
2024-02-07 11:20:09 +0000

    CoinCap

--- app/src/main/java/com/example/coincapapp/Api/CoinCapInterface.kt
+++ app/src/main/java/com/example/coincapapp/Api/CoinCapInterface.kt
@@ -1,20 +1,12 @@
 package com.example.coincapapp.Api
 
 import com.example.coincapapp.Constants
-import com.example.coincapapp.data.CoinCap
 import com.example.coincapapp.data.CoinCapList
 import retrofit2.http.GET
-import retrofit2.http.Path
-
-private const val EXTENSION_URL = "v2/rates"
 
 interface CoinCapInterface {
 
     @GET(Constants.EXTENSION_URL)
     suspend fun getCoinCap() : CoinCapList
 
-    @GET("$EXTENSION_URL/{id}")
-    suspend fun getCoinCapDetails(
-        @Path("id") id : String?
-    ) : CoinCap
 }
--- app/src/main/java/com/example/coincapapp/repo/CoinCapRepo.kt
+++
@@ -1,8 +0,0 @@
-package com.example.coincapapp.repo
-
-import com.example.coincapapp.data.CoinCap
-
-interface CoinCapRepo {
-
-    suspend fun getCoinCap() : List<CoinCap>
-}
